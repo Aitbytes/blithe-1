254cb871a81e4b429c7a7e2439aaf5fee985eb5e
a8taleb <a8taleb@nixos-hp>
2025-04-05 15:50:46 +0200

    remove commented out pieces

--- infra-terraform/cloudflare.tf
+++ infra-terraform/cloudflare.tf
@@ -2,19 +2,6 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-# Create/Update Cloudflare DNS record
-# resource "cloudflare_record" "dns_record" {
-#   name    = var.dns_record_name
-#   zone_id = var.cloudflare_zone_id
-#   type    = "A"
-#   value   = hcloud_server.nodes[0].ipv4_address  # Assuming you want to use the first node's IP
-#   ttl     = 300
-#
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
 resource "cloudflare_dns_record" "main_dns_record" {
   zone_id = var.cloudflare_zone_id
   # comment = "Domain verification record"
