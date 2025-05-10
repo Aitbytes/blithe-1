c812c9388f527c971d65792a61dccc4dfe7fe49f
a8taleb <a8taleb@nixos-hp>
2025-05-10 15:08:45 +0200

    replace cloudflare variable with vault kv reference

--- infra-terraform/cloudflare.tf
+++ infra-terraform/cloudflare.tf
@@ -1,12 +1,12 @@
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+  api_token = local.cloudflare_config.api_token
 }
 
 resource "cloudflare_dns_record" "main_dns_record" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = local.cloudflare_config.zone_id
   # comment = "Domain verification record"
   content = hcloud_server.nodes[0].ipv4_address  # Assuming you want to use the first node's IP
-  name = var.dns_record_name
+  name = local.cloudflare_config.record_name
   proxied = false
   ttl = 3600
   type = "A"
@@ -16,7 +16,7 @@ resource "cloudflare_dns_record" "main_dns_record" {
 }
 
 resource "cloudflare_dns_record" "wildcard_subdomain" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = local.cloudflare_config.zone_id
   content = hcloud_server.nodes[0].ipv4_address
   name    = "*"
   proxied = false
